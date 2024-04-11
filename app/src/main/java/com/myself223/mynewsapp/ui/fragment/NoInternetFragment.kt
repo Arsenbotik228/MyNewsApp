45cd7cae2b4fa3b4a46f2d5e6624a70c343aff3f
Arsenbotik228 <147414597+Arsenbotik228@users.noreply.github.com>
2024-04-11 17:35:19 +0600

    Проверка на интирьнет

--- app/src/main/java/com/myself223/mynewsapp/ui/fragment/NoInternetFragment.kt
+++ app/src/main/java/com/myself223/mynewsapp/ui/fragment/NoInternetFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.myself223.mynewsapp.R
 import com.myself223.mynewsapp.databinding.FragmentNoInternetBinding
@@ -29,6 +30,7 @@ class NoInternetFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.btnCheck.setOnClickListener{
             if (!isNetworkAvailable(requireContext())) {
+                Toast.makeText(requireContext(),"Все еще нет интеренета",Toast.LENGTH_SHORT).show()
 
 
             }else{
